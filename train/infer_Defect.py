4a5fb6f76d46182011055a859ed0f4bd76346512
GT_Console <1029550448@qq.com>
2019-06-01 23:42:11 +0800

    imgTools update

--- train/infer_Defect.py
+++ train/infer_Defect.py
@@ -9,6 +9,7 @@ try:
     from PIL import Image
     import cv2 as cv
     from imgTool import imgdetection as imgdetection
+    from imgTool import readIMGInDir
     import traceback
 except:
     print(traceback.format_exc())
@@ -50,19 +51,28 @@ def readIMG(imgFilePath):
     im = img_obj.three2one()
     im0 = Image.fromarray(cv.cvtColor(im, cv.COLOR_BGR2RGB))
     im0 = im0.resize((IMG_H, IMG_W), Image.ANTIALIAS)
-    im0.save("./temp2.jpg")
     # im.show()
     im = np.array(im0).reshape(1, 3, IMG_W, IMG_H).astype(np.float32)
     # im = im / 255.0 * 2.0 - 1.0
     return im,im0
 
-
-[inference_program, feed_target_names, fetch_targets] = fluid.io.load_inference_model(dirname=testModelPath,
+try:
+    [inference_program, feed_target_names, fetch_targets] = fluid.io.load_inference_model(dirname=testModelPath,
                                                                                       executor=exe)
+except:
+    print("无法读取模型文件")
+    temp = input("按任意键退出")
+    exit("发现异常，退出程序")
 imgpath = input("模型准备完毕，请输入图片位置(请勿使用中文目录)__").replace("\\", "/")
 print("图片路径为：" + imgpath)
+
+files=readIMGInDir(imgpath)
+print("图片数量为：",len(files))
+imgL=[]
 try:
-    img,imF = readIMG(imgpath)
+    for file in files:
+        img,imF = readIMG(file)
+        imgL.append(img)
 except:
     print(traceback.format_exc())
     print("请检查图片路径是否正确")
@@ -71,12 +81,13 @@ except:
 
 print("图片读取成功，开始预测...")
 try:
-    results = exe.run(inference_program,
-                      feed={feed_target_names[0]: img},
-                      fetch_list=fetch_targets)
-    lab = np.argsort(results)[0][0][-1]
+    for img in imgL:
+        results = exe.run(inference_program,
+                          feed={feed_target_names[0]: img},
+                          fetch_list=fetch_targets)
+        lab = np.argsort(results)[0][0][-1]
 
-    print(key[int(lab)])
+        print(key[int(lab)])
 
 except:
     print(traceback.format_exc())
@@ -84,6 +95,6 @@ except:
     exit("请检查配置环境，Fluid启动失败")
 print("渲染图片ing...")
 
-imF.show()
+#imF.show()
 
 input("运行结束")
