dbc344f05acec30d5b19af61fbe7ce75f35d5d6c
GT_Console <1029550448@qq.com>
2019-06-11 12:59:41 +0800

    Icnet--OK

--- train/fabricNet.py
+++ train/fabricNet.py
@@ -32,78 +32,52 @@ class FabricNet:
         :param input:
         :return:
         '''
-        cp_1 = fluid.nets.simple_img_conv_pool(
+        cp_1 = fluid.layers.conv2d(
             input=input,
-            filter_size=3,  # 3x3滤波器
-            num_filters=256,  # 卷积核数量
-            pool_size=2,  # 池化x2
-            pool_stride=2,  # 池化步长2
-            conv_padding=1,  # 填充
-            act="relu")
+            num_filters=256,
+            filter_size=3,
+            act="relu",
+            padding=1
+        )
         bn_1 = fluid.layers.batch_norm(cp_1)
-        cp_2 = fluid.nets.simple_img_conv_pool(
-            input=bn_1,
-            filter_size=3,  # 3x3滤波器
-            num_filters=256,  # 卷积核数量
-            pool_size=2,  # 池化x2
-            pool_stride=2,  # 池化步长2
-            conv_padding=1,  # 填充
-            act="relu")
-        bn_2 = fluid.layers.batch_norm(cp_2)
-        cp_3 = fluid.nets.simple_img_conv_pool(
-            input=bn_2,
-            filter_size=3,  # 3x3滤波器
-            num_filters=512,  # 卷积核数量
-            pool_size=2,  # 池化x2
-            pool_stride=2,  # 池化步长2
-            conv_padding=2,  # 填充
-            act="relu")
-        bn_3 = fluid.layers.batch_norm(cp_3)
-        # cp_4 = fluid.nets.simple_img_conv_pool(
-        #     input=bn_3,
+        # cp_2 = fluid.nets.simple_img_conv_pool(
+        #     input=bn_1,
+        #     filter_size=3,  # 3x3滤波器
+        #     num_filters=256,  # 卷积核数量
+        #     pool_size=2,  # 池化x2
+        #     pool_stride=2,  # 池化步长2
+        #     conv_padding=1,  # 填充
+        #     act="relu")
+        # bn_2 = fluid.layers.batch_norm(cp_2)
+        # cp_3 = fluid.nets.simple_img_conv_pool(
+        #     input=bn_2,
         #     filter_size=3,  # 3x3滤波器
         #     num_filters=512,  # 卷积核数量
         #     pool_size=2,  # 池化x2
         #     pool_stride=2,  # 池化步长2
         #     conv_padding=2,  # 填充
         #     act="relu")
-        # bn_4 = fluid.layers.batch_norm(cp_4)
-        # cp_5 = fluid.nets.simple_img_conv_pool(
-        #     input=bn_4,
-        #     filter_size=3,  # 3x3滤波器
-        #     num_filters=1024,  # 卷积核数量
-        #     pool_size=2,  # 池化x2
-        #     pool_stride=2,  # 池化步长2
-        #     conv_padding=4,  # 填充
-        #     act="relu")
-        # bn_5 = fluid.layers.batch_norm(cp_5)
-        return bn_3
+        # bn_3 = fluid.layers.batch_norm(cp_3)
+
+        return bn_1
 
     def net(self, input):
-        path1_input = self.interp(input, 0.065)
-        path2_input = self.interp(input, 0.3)
-        path3_input = self.interp(input, 0.8)
+        path1_input = self.interp(input, 0.075)  # x1
+        path2_input = self.interp(input, 0.3)  # x4
+        path3_input = self.interp(input, 0.6)  # x8
         path1_conv = self.conv_share(path1_input)
         path2_conv = self.conv_share(path2_input)
         # path2
         p2_cp_4 = fluid.nets.simple_img_conv_pool(
             input=path2_conv,
             filter_size=3,  # 3x3滤波器
-            num_filters=512,  # 卷积核数量
-            pool_size=2,  # 池化x2
-            pool_stride=2,  # 池化步长2
-            conv_padding=2,  # 填充
-            act="relu")
-        p2_bn_4 = fluid.layers.batch_norm(p2_cp_4)
-        p2_cp_5 = fluid.nets.simple_img_conv_pool(
-            input=p2_bn_4,
-            filter_size=3,  # 3x3滤波器
             num_filters=1024,  # 卷积核数量
             pool_size=2,  # 池化x2
             pool_stride=2,  # 池化步长2
-            conv_padding=2,  # 填充
+            conv_padding=1,  # 填充
             act="relu")
-        p2_bn_5 = fluid.layers.batch_norm(p2_cp_5)
+        p2_bn_4 = fluid.layers.batch_norm(p2_cp_4)
+
         # path3
         path3_conv = self.conv_share(path3_input)
         p3_cp_4 = fluid.nets.simple_img_conv_pool(
@@ -112,14 +86,32 @@ class FabricNet:
             num_filters=512,  # 卷积核数量
             pool_size=2,  # 池化x2
             pool_stride=2,  # 池化步长2
-            conv_padding=2,  # 填充
+            conv_padding=1,  # 填充
             act="relu")
         p3_bn_4 = fluid.layers.batch_norm(p3_cp_4)
+        p3_cp_5 = fluid.nets.simple_img_conv_pool(
+            input=p3_bn_4,
+            filter_size=3,  # 3x3滤波器
+            num_filters=512,  # 卷积核数量
+            pool_size=2,  # 池化x2
+            pool_stride=2,  # 池化步长2
+            conv_padding=1,
+            act="relu")
 
+        p3_bn_5 = fluid.layers.batch_norm(p3_cp_5)
 
-        # concat = fluid.layers.concat(
-        #    [path1_conv, path2_conv, path3_conv], axis=1)
-        print("path1", path1_conv.shape)
-        print("path2", p2_bn_5.shape)
-        print("path3", p3_bn_4.shape)
-        return 0
+        p3_final = p3_bn_5
+        p2_final = p2_bn_4
+        p1_final = fluid.layers.resize_bilinear(path1_conv, out_shape=p2_bn_4.shape[-2:])
+        print("path1", p1_final.shape)
+        print("path2", p2_final.shape)
+        print("path3", p3_final.shape)
+
+        # 需要改
+        out = fluid.layers.concat(
+            [p1_final, p2_final, p3_final], axis=1)
+        out = fluid.layers.conv2d(out, 10, 1, 1)
+        out_final = fluid.layers.resize_bilinear(out, out_shape=[i//4 for i in input.shape[-2:]])
+        print("out", out.shape)
+        print("out_F", out_final.shape)
+        return out
--- train/train_for_Defect.py
+++ train/train_for_Defect.py
@@ -4,13 +4,11 @@ import numpy as np
 from PIL import Image
 import cv2 as cv
 import matplotlib.pyplot as plt
-from torNN import TorNN
-from imgTool import readIMGInDir as readIMGInDir
-from imgTool import imgdetection as imgdetection
-from osTools import mkdirL as mkdirL
-from osTools import readDirName as readDirName
-from pylab import mpl
+from fabricReader import train as dataReader
 from fabricNet import FabricNet
+from pylab import mpl
+from paddle.fluid.layers.learning_rate_scheduler import _decay_step_counter
+from paddle.fluid.initializer import init_on_cpu
 
 mpl.rcParams['font.sans-serif'] = ['SimHei']  # 解决PIL显示乱码问题
 
@@ -20,7 +18,11 @@ place = fluid.CPUPlace()
 TRAIN_DATA_SHAPE = (3, 1200, 2448)
 C, IMG_H, IMG_W = TRAIN_DATA_SHAPE  # 输入网络图像大小
 TRAINNUM = 100  # 训练次数
-READIMG = 50  # 每次读取图片数量
+READIMG = 5  # 每次读取图片数量
+class_dim=10
+LEARNING_RATE = 0.003  # 学习率
+POWER = 0.9  # 学习率控制
+TOTAL_STEP = 10  # 训练轮数
 
 # 指定路径
 # 路径除root外均不带"/"后缀
@@ -32,37 +34,17 @@ labelFilePath = path + 'trainData/Classified/label.txt'
 print("模型文件夹路径" + baseModelPath)
 print("原始图片文件夹路径" + imgPath)
 
+
+def poly_decay():
+    global_step = _decay_step_counter()
+    with init_on_cpu():
+        decayed_lr = LEARNING_RATE * (fluid.layers.pow(
+            (1 - global_step / TOTAL_STEP), POWER))
+    return decayed_lr
 # 参数初始化
 exe = fluid.Executor(place)
 
 # 加载数据
-dirL = readDirName(imgPath)  # 分类图片路径数据
-
-
-def dataReader():
-    def reader():
-        '''
-        数据读取器
-        目录结构为：./ 数字标签1 /所有该标签的图片
-                  ./ 数字标签2 /所有该标签的图片
-                  ...
-        :return:
-        '''
-        for label in dirL:
-            imgFilePathL = readIMGInDir(imgPath + "/" + str(label) + "/")
-            for imgFilePath in imgFilePathL:
-                # img_obj = imgdetection(imgFilePath)
-                # im = img_obj.three2one()
-                # im = Image.fromarray(cv.cvtColor(im, cv.COLOR_BGR2RGB))
-                # im = im.resize((IMG_H, IMG_W), Image.ANTIALIAS)
-                # im.show()
-                im = Image.open(imgFilePath)
-                im = np.array(im).reshape(3, IMG_W, IMG_H).astype(np.float32)
-                # im = im / 255.0 * 2.0 - 1.0
-                yield im, int(label)
-
-    return reader
-
 
 # 新建项目
 defectProgram = fluid.Program()
@@ -70,25 +52,41 @@ startup = fluid.Program()  # 默认启动程序
 
 # 编辑项目
 with fluid.program_guard(main_program=defectProgram, startup_program=startup):
-    x_f = fluid.layers.data(name="x_f", shape=[C, IMG_H, IMG_W], dtype='float32')
-    label_f = fluid.layers.data(name="label_f", shape=[1], dtype="int64")
-    net = FabricNet(class_dim=10)
-    net_x = net.net(x_f)
+    no_grad_set = []
+    def create_loss(predict, label, mask, num_classes):
+        predict = fluid.layers.transpose(predict, perm=[0, 2, 3, 1])
+        predict = fluid.layers.reshape(predict, shape=[-1, num_classes])
+        label = fluid.layers.reshape(label, shape=[-1, 1])
+        predict = fluid.layers.gather(predict, mask)
+        label = fluid.layers.gather(label, mask)
+        label = fluid.layers.cast(label, dtype="int64")
+        loss = fluid.layers.softmax_with_cross_entropy(predict, label)
+        no_grad_set.append(label.name)
+        return fluid.layers.reduce_mean(loss)
+
+
+    image = fluid.layers.data(name="x_f", shape=[C, IMG_H, IMG_W], dtype='float32')
+    mask_sub = fluid.layers.data(name='mask_sub1', shape=[-1], dtype='int32')
+    label_sub = fluid.layers.data(name='label_sub1', shape=[1], dtype='int32')
+
+    net = FabricNet(class_dim=class_dim)
+    net_x = net.net(image)
     # 定义损失函数
-    cost_Base_f = fluid.layers.cross_entropy(input=net_x, label=label_f)
-    avg_cost_Base_f = fluid.layers.mean(fluid.layers.abs(cost_Base_f))
-    acc = fluid.layers.accuracy(input=net_x, label=label_f, k=1)
+    cost_Base_f = create_loss(net_x, label_sub, mask_sub, class_dim)
+
     # final_programT = final_program.clone(for_test=True)
     # 定义优化方法
-    sgd_optimizer_f = fluid.optimizer.Adam(learning_rate=0.01)
-    sgd_optimizer_f.minimize(avg_cost_Base_f)
+    regularizer = fluid.regularizer.L2Decay(0.0001)  # 权重衰减 防止过拟合
+    optimizer = fluid.optimizer.Momentum(
+        learning_rate=poly_decay(), momentum=0.9, regularization=regularizer)
+    _, params_grads = optimizer.minimize(cost_Base_f, no_grad_set=no_grad_set)
 
 # 数据传入设置
 
 prebatch_reader = paddle.batch(
     reader=paddle.reader.shuffle(dataReader(), 100),
     batch_size=READIMG)
-prefeeder = fluid.DataFeeder(place=place, feed_list=[x_f, label_f])
+prefeeder = fluid.DataFeeder(place=place, feed_list=[image, label_sub,mask_sub])
 
 # 准备训练
 exe.run(startup)
@@ -103,27 +101,6 @@ for train_num, i in enumerate(range(TRAINNUM)):
         # 获取训练数据
         outs = exe.run(program=defectProgram,
                        feed=prefeeder.feed(data),
-                       fetch_list=[label_f, avg_cost_Base_f, acc, net_x])
-        print(train_num, outs[1], outs[2])
-        try:
-            accsum.append(int(outs[2]))
-            losssum.append(int(outs[1]))
-        except:
-            continue
-        with open("./log.txt", "w") as f:
-            f.writelines(str(train_num) + str(outs[1]) + "\n")
-        accMean = sum(accsum) / len(accsum)
-        lossMean = sum(losssum) / len(losssum)
-        if accMean >= 0.7 and len(accsum) >= 6 or train_num == 20:
-            fluid.io.save_inference_model(baseModelPath + str(batch_id) + str(accMean), ['x_f'], [net_x], exe,
-                                          main_program=defectProgram)
+                       fetch_list=[cost_Base_f])
+        print(train_num, outs[0])
 
-    plt.figure(1)
-    plt.title('布料识别指标-损失')
-    plt.xlabel('迭代次数')
-    plt.plot(train_num, lossMean)
-    plt.figure(2)
-    plt.plot(train_num, accMean)
-    plt.title('布料识别指标-正确率')
-    plt.xlabel('迭代次数')
-    plt.show()
